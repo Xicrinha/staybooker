40831fd91b472029ab02894398e8299d43ac270b
Matias Monteiro <matiasmonteiro15@gmail.com>
2023-07-24 20:43:33 -0300

    alteração no GuestDTO para que ele poça receber json junto com os atributos do Address, e alterando tambem o GuestMapper para que ele converta os atributos do GuestDTO que representam um endereço para a sua devida classe Address

--- backend/src/main/java/com/xikra/staybooker/mapper/GuestMapper.java
+++ backend/src/main/java/com/xikra/staybooker/mapper/GuestMapper.java
@@ -3,11 +3,22 @@ package com.xikra.staybooker.mapper;
 import com.xikra.staybooker.domain.Guest;
 import com.xikra.staybooker.model.GuestDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface GuestMapper {
 
-    GuestDTO guestToGuestDTO (Guest guest);
+    @Mapping(target = "street" , source = "address.street")
+    @Mapping(target = "number" , source = "address.number")
+    @Mapping(target = "city" , source = "address.city")
+    @Mapping(target = "state" , source = "address.state")
+    @Mapping(target = "zipcode" , source = "address.zipcode")
+    GuestDTO toDTO (Guest guest);
 
-    Guest guestDTOToGuest(GuestDTO guestDTO);
+    @Mapping(target = "address.street" , source = "street")
+    @Mapping(target = "address.number" , source = "number")
+    @Mapping(target = "address.city" , source = "city")
+    @Mapping(target = "address.state" , source = "state")
+    @Mapping(target = "address.zipcode" , source = "zipcode")
+    Guest toEntity(GuestDTO guestDTO);
 }
--- backend/src/main/java/com/xikra/staybooker/model/GuestDTO.java
+++ backend/src/main/java/com/xikra/staybooker/model/GuestDTO.java
@@ -3,6 +3,7 @@ package com.xikra.staybooker.model;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.HashSet;
@@ -20,7 +21,18 @@ public class GuestDTO {
     private String firstname;
     @NotBlank(message = "Last name is required")
     private String lastname;
-    private AddressDTO adress;
+    @NotBlank(message = "Street name is required")
+    private String street;
+    @NotBlank(message = "Number is required")
+    private String number;
+    @NotBlank(message = "The city is required")
+    private String city;
+    @NotBlank(message = "The state is required")
+    @Size(min = 2, max = 2, message = "State must be exactly 2 characters long")
+    private String state;
+    @NotBlank(message = "The Zip Code is required")
+    @Size(min = 5, max = 10, message = "The Zip Code must be between 5 and 10 characters")
+    private String zipcode;
     @NotBlank(message = "Phone number is required")
     @Pattern(regexp = "\\(\\d{2}\\)\\s\\d{4,5}-\\d{4}", message = "The phone number must be in the format (XX) XXXX-XXXX or (XX) XXXXX-XXXX")
     private String phoneNumber;
