4546bb92e069feaf942683087c910ebe6bf752e3
Matias Monteiro <matiasmonteiro15@gmail.com>
2023-08-23 00:19:47 -0300

    refatorando a classe AmenityService, transformando ela em uma interface e implementando um AmenityServiceImpl

--- backend/src/main/java/com/xikra/staybooker/service/AmenityService.java
+++ backend/src/main/java/com/xikra/staybooker/service/AmenityService.java
@@ -1,43 +1,19 @@
 package com.xikra.staybooker.service;
 
 import com.xikra.staybooker.domain.Amenity;
-import com.xikra.staybooker.exceptions.NotFoundException;
-import com.xikra.staybooker.repository.AmenityRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
-@RequiredArgsConstructor
-public class AmenityService {
+public interface AmenityService {
 
-    private final AmenityRepository amenityRepository;
+    public Amenity createAmenity(Amenity amenity);
 
-    public Amenity createAmenity(Amenity amenity) {
-        return amenityRepository.save(amenity);
-    }
+    public List<Amenity> getAllAmenities();
 
-    public List<Amenity> getAllAmenities() {
-        return amenityRepository.findAll();
-    }
+    public Optional<Amenity> getAmenityById(Long id);
 
-    public Optional<Amenity> getAmenityById(Long id) {
-        return amenityRepository.findById(id);
-    }
+    public Amenity updateAmenity(Amenity amenity, Long id);
 
-    public Amenity updateAmenity(Amenity amenity, Long id) {
-        Amenity existingAmenity = amenityRepository.findById(id).orElseThrow(NotFoundException::new);
-        existingAmenity.setName(amenity.getName());
-        return amenityRepository.save(existingAmenity);
-    }
-
-    public boolean deleteAmenity(Long id) {
-        if(amenityRepository.existsById(id)){
-            amenityRepository.deleteById(id);
-            return true;
-        }
-        return false;
-    }
+    public boolean deleteAmenity(Long id);
 }
