0b5b85a607b4ebb48164c2119746ba9650b8d2bd
Matias Monteiro <matiasmonteiro15@gmail.com>
2023-08-29 23:38:20 -0300

    Implementando metodos toDTOList e toEntityList na interface AmenityMapper

--- backend/src/main/java/com/xikra/staybooker/mapper/AmenityMapper.java
+++ backend/src/main/java/com/xikra/staybooker/mapper/AmenityMapper.java
@@ -4,10 +4,33 @@ import com.xikra.staybooker.domain.Amenity;
 import com.xikra.staybooker.model.AmenityDTO;
 import org.mapstruct.Mapper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Mapper
 public interface AmenityMapper {
 
     AmenityDTO toDTO(Amenity amenity);
 
     Amenity toEntity(AmenityDTO amenityDTO);
+
+    default List<Amenity> toEntityList(List<AmenityDTO> amenityDTOList){
+        List<Amenity> amenityList = new ArrayList<>();
+
+        for(AmenityDTO amenityDTO : amenityDTOList){
+            amenityList.add(toEntity(amenityDTO));
+        }
+
+        return amenityList;
+    }
+
+    default List<AmenityDTO> toDTOList(List<Amenity> amenityList){
+        List<AmenityDTO> amenityDTOList = new ArrayList<>();
+
+        for(Amenity amenity : amenityList){
+            amenityDTOList.add(toDTO(amenity));
+        }
+
+        return amenityDTOList;
+    }
 }
