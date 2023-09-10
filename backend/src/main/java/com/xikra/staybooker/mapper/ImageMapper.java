f52ca59baf52261f43aca82ff1018aa4988e0f04
Matias Monteiro <matiasmonteiro15@gmail.com>
2023-09-09 23:57:46 -0300

    implementando o metodo toDTOPage e toEntityPage na interface ImageMapper

--- backend/src/main/java/com/xikra/staybooker/mapper/ImageMapper.java
+++ backend/src/main/java/com/xikra/staybooker/mapper/ImageMapper.java
@@ -3,6 +3,11 @@ package com.xikra.staybooker.mapper;
 import com.xikra.staybooker.domain.Image;
 import com.xikra.staybooker.model.ImageDTO;
 import org.mapstruct.Mapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mapper
 public interface ImageMapper {
@@ -10,4 +15,24 @@ public interface ImageMapper {
     ImageDTO toDTO(Image image);
 
     Image toEntity(ImageDTO imageDTO);
+
+    default Page<ImageDTO> toDTOPage(Page<Image> imagePage){
+        List<ImageDTO> imageDTOList = new ArrayList<>();
+
+        for(Image image : imagePage){
+            imageDTOList.add(toDTO(image));
+        }
+
+        return new PageImpl<>(imageDTOList);
+    }
+
+    default Page<Image> toEntityPage(Page<ImageDTO> imageDTOPage){
+        List<Image> imageList = new ArrayList<>();
+
+        for(ImageDTO imageDTO : imageDTOPage){
+            imageList.add(toEntity(imageDTO));
+        }
+
+        return new PageImpl<>(imageList);
+    }
 }
